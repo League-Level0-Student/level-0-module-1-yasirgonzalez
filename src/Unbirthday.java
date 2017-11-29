9b1aa06414a17eff6bea83d4bfa56bb0f17dbe5f
gonzalez.yasir <gonzalez.yasir@SEMS-259-11.SEMS259.LAB>
2017-11-29 14:54:22 -0800

    unbirthday, remarkable

---
+++ src/Unbirthday.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	
+	String birthday=JOptionPane.showInputDialog("What's your birthday");
+	if(birthday.equals("11/29")) {
+	JOptionPane.showMessageDialog(null,"Happy Brithday");
+	}
+	else{
+		JOptionPane.showMessageDialog(null,"Happy unbrithday");
+	}
+
+}
+}
---
+++ src/remarkable.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class remarkable {
+public static void main(String[] args) {
+	String name=JOptionPane.showInputDialog("what is your name");
+	if(name.equals("Victor")) {
+		JOptionPane.showMessageDialog(null,"Hello Victor, you are great at codeing and a great base player");
+	}
+	else if(name.equals("Yasir")) {
+		JOptionPane.showMessageDialog(null,"Hello Yasir, you are the best tumpet player and the boss ;)");
+		
+	}else if(name.equals("Max")) {
+		JOptionPane.showMessageDialog(null,"Hello Max, you are a great coder you type like if a cheetah was running on you key board");
+    }else if(name.equals("Emil")) {
+	JOptionPane.showMessageDialog(null,"Hello Emil, you are a great sax player");
+    }else if(name.equals("Callum")) {
+		JOptionPane.showMessageDialog(null,"Hello Callum, you are a great baritone player");
+    }else if(name.equals("Yasir")) {
+			JOptionPane.showMessageDialog(null,"Hello Lauren, you are a great sax player");
+    }else if(name.equals("Yasir")) {
+				JOptionPane.showMessageDialog(null,"Hello Cruz, you are good of staing though");
+}
+}
+}
