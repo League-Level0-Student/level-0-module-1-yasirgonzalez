6556f5df3c79efd9479d88c7478d575da4bb002c
gonzalez.yasir <gonzalez.yasir@SEMS-259-18.SEMS259.LAB>
2017-12-06 14:54:45 -0800

    remarkable, SecretMessage

--- src/Unbirthday.java
+++ src/Unbirthday.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class Unbirthday {
 public static void main(String[] args) {
-	//iuhuyhuyhuy
+	
 	String birthday=JOptionPane.showInputDialog("What's your birthday");
 	if(birthday.equals("11/29")) {
 	JOptionPane.showMessageDialog(null,"Happy Brithday");
--- src/remarkable.java
+++ src/remarkable.java
@@ -1,25 +1,27 @@
 import javax.swing.JOptionPane;
 
 public class remarkable {
-	//uhuhuhuyh
+	
 public static void main(String[] args) {
 	String name=JOptionPane.showInputDialog("what is your name");
 	if(name.equals("Victor")) {
 		JOptionPane.showMessageDialog(null,"Hello Victor, you are great at codeing and a great base player");
 	}
 	else if(name.equals("Yasir")) {
-		JOptionPane.showMessageDialog(null,"Hello Yasir, you are the best tumpet player and the boss ;)");
+		JOptionPane.showMessageDialog(null,"Hello Yasir, you are the best tumpet player and the boss. Also your name means rich ;)");
 		
 	}else if(name.equals("Max")) {
 		JOptionPane.showMessageDialog(null,"Hello Max, you are a great coder you type like if a cheetah was running on you key board");
     }else if(name.equals("Emil")) {
-	JOptionPane.showMessageDialog(null,"Hello Emil, you are a great sax player");
+	JOptionPane.showMessageDialog(null,"Hello Emil, good job, you are a great sax player");
     }else if(name.equals("Callum")) {
-		JOptionPane.showMessageDialog(null,"Hello Callum, you are a great baritone player");
-    }else if(name.equals("Yasir")) {
-			JOptionPane.showMessageDialog(null,"Hello Lauren, you are a great sax player");
-    }else if(name.equals("Yasir")) {
-				JOptionPane.showMessageDialog(null,"Hello Cruz, you are good of staing though");
-}
+		JOptionPane.showMessageDialog(null,"Hello Callum, amazing job, you are a great baritone player");
+    }else if(name.equals("Lauren")) {
+			JOptionPane.showMessageDialog(null,"Hello Lauren, great job, you are a great sax player");
+    }else if(name.equals("Cruz")) {
+				JOptionPane.showMessageDialog(null,"Hello Cruz, good job you are good at beging though"); 
+    }else {
+    	JOptionPane.showMessageDialog(null,"hello, great job, else you look great");
+    }
 }
 }
