6bf700cc671f14ca162193dfa566fec3a41953da
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-06-05 18:29:40 -0700

    BadgerSong, MyAge

---
+++ src/MyAge.java
@@ -0,0 +1,8 @@
+
+public class MyAge {
+	public static void main(String[] args) {
+		for(int i=2005; i<2019; i++) {
+			System.out.println(i-2005+","+ i);	
+			}
+	}
+}
---
+++ src/badgersong.java
@@ -0,0 +1,19 @@
+
+public class badgersong {
+	public static void main(String[] args) {
+	String animal="Badger";
+	String fungus="Mushroom";
+	String scary="Snake";
+	for(int j=0;j<3; j++) {
+	for(int i=0; i<16; i++) {
+	System.out.print(animal+" ");
+		}
+	for(int i=0;i<2; i++) {
+	System.out.println(fungus+" ");	
+	}
+	}
+for(int i=0;i<5; i++) {
+	System.out.print(scary+" ");
+}
+	}
+	}
